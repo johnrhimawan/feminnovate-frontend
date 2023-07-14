c6c85f2d769d02ec5f875effb2ba5abecf8f6c90
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-07-15 00:35:47 +0800

    Display job listing page in App.jsx

--- feminnovate-frontend/src/App.jsx
+++ feminnovate-frontend/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import styles from './style'
+import JobListingPage from './pages/JobListingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className={`${styles.heading1} text-yellow`}>hello</h1>
+    <JobListingPage/>
   )
 }
 
