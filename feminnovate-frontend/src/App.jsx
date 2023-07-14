3583bba3a586f8304d39778c636f554f8a81cc00
Jason Christopher <jason.christopher@u.nus.edu>
2023-07-15 01:09:16 +0800

    Create Navbar

--- feminnovate-frontend/src/App.jsx
+++ feminnovate-frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import styles from './style'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import styles from "./style";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <h1 className={`${styles.heading1} text-yellow`}>hello</h1>
-  )
+    return (
+        // <h1 className={`${styles.heading1} text-yellow`}>hello</h1>
+        <div>
+            <Navbar />
+        </div>
+    );
 }
 
-export default App
+export default App;
