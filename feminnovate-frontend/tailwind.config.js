52f96d6f37a0d9de4a4949150560ddc83c128e93
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-07-14 22:42:45 +0800

    Configure tailwind css

---
+++ feminnovate-frontend/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+      tall: {'raw': '(min-height: 768px) and (min-width: 768px)'}
+    },
+    colors: {
+      'white': '#FEFEFE',
+      'pink': '#FFB5C6',
+      'blue': '#97B4FF',
+      'green': '#9CFFCA',
+      'yellow': '#FFDD86',
+      'black': '#1E1E1E',
+      'purple': '#D5A0FF',
+      'grey': '#D9D9D9',
+    },
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
