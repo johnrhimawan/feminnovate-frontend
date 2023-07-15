a5d017b8f0b071fc1960d49f0d7e8ee8adbd698b
gwynethguo <e0905362@u.nus.edu>
2023-07-15 13:58:23 +0800

    Route to landing page

--- feminnovate-frontend/src/App.jsx
+++ feminnovate-frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
-import JobListingPage from './pages/JobListingPage'
-import Navbar from './components/Navbar'
+import "./App.css";
+import JobListingPage from "./pages/JobListingPage";
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-
   return (
     <>
-      <Navbar />
-      <JobListingPage />
+      <LandingPage />
     </>
-  )
+  );
 }
 
 export default App;
