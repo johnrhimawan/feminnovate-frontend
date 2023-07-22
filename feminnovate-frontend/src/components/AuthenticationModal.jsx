2400d49fa6544d6be9644616a9903e93959df5bf
gwynethguo <e0905362@u.nus.edu>
2023-07-22 18:16:41 +0800

    Separate into components

--- feminnovate-frontend/src/components/AuthenticationModal.jsx
+++ feminnovate-frontend/src/components/AuthenticationModal.jsx
@@ -5,6 +5,106 @@ import show from "../assets/show.png";
 import close from "../assets/close.png";
 import styles from "../style";
 
+const CloseButton = ({ onClick }) => {
+  return (
+    <button
+      className="w-3 sm:w-4 h-3 sm:h-4 m-auto -mt-3 -mr-4"
+      onClick={onClick}
+    >
+      <img src={close} />
+    </button>
+  );
+};
+
+const LogoWithTagline = () => {
+  return (
+    <div className="flex flex-col w-[100%] items-center mb-9">
+      <img className="w-[75%]" src={logo} />
+      <div className={`${styles.subheading5} -mt-1`}>
+        Building Careers For Women
+      </div>
+    </div>
+  );
+};
+
+const Input = ({ label, id, name, type, value, onChange }) => {
+  return (
+    <div className="flex flex-col w-[100%] items-left mb-5">
+      <label
+        htmlFor={id}
+        className={`${styles.subheading5} -mt-1 text-black/50 w-full ml-1`}
+      >
+        {label}
+      </label>
+      <input
+        id={id}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        className="border border-grey rounded-md w-full h-14 px-3 py-2"
+      ></input>
+    </div>
+  );
+};
+
+const PasswordInput = ({
+  label,
+  id,
+  name,
+  type,
+  value,
+  onChange,
+  onClick,
+  showInput,
+}) => {
+  return (
+    <div className="flex flex-col w-[100%] items-left">
+      <label
+        htmlFor={id}
+        className={`${styles.subheading5} -mt-1 text-black/50 w-full ml-1`}
+      >
+        {label}
+      </label>
+      <div className="flex justify-center items-center mb-5 relative">
+        <input
+          id={id}
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+          className="border border-grey rounded-md w-full h-14 pl-3 pr-12 py-2"
+        ></input>
+        <button
+          className="absolute right-0 ml-1 mr-3 px-1 items-center"
+          onClick={onClick}
+        >
+          {showInput ? (
+            <img className="w-6" src={hide} />
+          ) : (
+            <img className="w-6" src={show} />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const SwitchMessage = ({ question, hypertext, onClick }) => {
+  return (
+    <div>
+      {question}{" "}
+      <a
+        className="text-blue underline underline-offset-2"
+        href="#"
+        onClick={onClick}
+      >
+        {hypertext}
+      </a>
+    </div>
+  );
+};
+
 const AuthenticationModal = ({
   openSignUpModal,
   openLogInModal,
@@ -60,86 +160,38 @@ const AuthenticationModal = ({
   return (
     <div className="flex flex-col w-full h-full bg-white rounded-3xl p-10 items-center justify-around">
       <div className="flex w-full h-full">
-        <button
-          className="w-3 sm:w-4 h-3 sm:h-4 m-auto -mt-3 -mr-4"
-          onClick={handleCloseModal}
-        >
-          <img src={close} />
-        </button>
+        <CloseButton onClick={handleCloseModal}></CloseButton>
       </div>
-      <div className="flex flex-col w-[100%] items-center">
-        <img className="w-[75%]" src={logo} />
-        <div className={`${styles.subheading5} -mt-1 mb-9`}>
-          Building Careers For Women
-        </div>
-      </div>
-      <div className="flex flex-col w-[100%] items-left">
-        <div
-          className={`${styles.subheading5} -mt-1 text-black/50 w-full ml-1`}
-        >
-          Email
-        </div>
-        <input
-          name="email"
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          className="border border-grey rounded-md w-full h-14 mb-5 px-3 py-2"
-        ></input>
-      </div>
-      <div className="flex flex-col w-[100%] items-left">
-        <div
-          className={`${styles.subheading5} -mt-1 text-black/50 w-full ml-1`}
-        >
-          Password
-        </div>
-        <div className="flex justify-center items-center mb-5 relative">
-          <input
-            name="password"
-            type={showPassword ? "text" : "password"}
-            value={password}
-            onChange={handlePasswordChange}
-            className="border border-grey rounded-md w-full h-14 pl-3 pr-12 py-2"
-          ></input>
-          <button
-            className="absolute right-0 ml-1 mr-3 px-1 items-center"
-            onClick={togglePassword}
-          >
-            {showPassword ? (
-              <img className="w-6" src={hide} />
-            ) : (
-              <img className="w-6" src={show} />
-            )}
-          </button>
-        </div>
-      </div>
+      <LogoWithTagline></LogoWithTagline>
+      <Input
+        label="Email"
+        id="email"
+        name="email"
+        type="email"
+        value={email}
+        onChange={handleEmailChange}
+      ></Input>
+      <PasswordInput
+        label="Password"
+        id="password"
+        name="password"
+        type={showPassword ? "text" : "password"}
+        value={password}
+        onChange={handlePasswordChange}
+        onClick={togglePassword}
+        showInput={showPassword ? 1 : 0}
+      ></PasswordInput>
       {openSignUpModal && (
-        <div className="flex flex-col w-[100%] items-left">
-          <div
-            className={`${styles.subheading5} -mt-1 text-black/50 text-left w-full ml-1`}
-          >
-            Confirm Password
-          </div>
-          <div className="flex justify-center items-center mb-5 relative">
-            <input
-              name="confirmPassword"
-              type={showConfirmPassword ? "text" : "password"}
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              className="border border-grey rounded-md w-full h-14 pl-3 pr-12 py-2"
-            ></input>
-            <button
-              className="absolute right-0 ml-1 mr-3 px-1 items-center"
-              onClick={toggleConfirmPassword}
-            >
-              {showConfirmPassword ? (
-                <img className="w-6" src={hide} />
-              ) : (
-                <img className="w-6" src={show} />
-              )}
-            </button>
-          </div>
-        </div>
+        <PasswordInput
+          label="Confirm Password"
+          id="confirmPassword"
+          name="confirmPassword"
+          type={showConfirmPassword ? "text" : "password"}
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          onClick={toggleConfirmPassword}
+          showInput={showConfirmPassword ? 1 : 0}
+        ></PasswordInput>
       )}
       <button
         className={`${styles.subheading4} bg-purple w-full px-12 py-3 my-5 border border-black rounded-full`}
@@ -149,27 +201,17 @@ const AuthenticationModal = ({
       </button>
       <div className={`${styles.paragraph1}`}>
         {openSignUpModal ? (
-          <div>
-            Have an account?{" "}
-            <a
-              className="text-blue underline underline-offset-2"
-              href="#"
-              onClick={handleSwitchToLogIn}
-            >
-              Login here.
-            </a>
-          </div>
+          <SwitchMessage
+            question="Have an account?"
+            hypertext="Sign up here."
+            onClick={handleSwitchToLogIn}
+          />
         ) : (
-          <div>
-            Don't have an account?{" "}
-            <a
-              className="text-blue underline underline-offset-2"
-              href="#"
-              onClick={handleSwitchToSignUp}
-            >
-              Sign up here.
-            </a>
-          </div>
+          <SwitchMessage
+            question="Don't have an account?"
+            hypertext="Login here."
+            onClick={handleSwitchToSignUp}
+          />
         )}
       </div>
     </div>
