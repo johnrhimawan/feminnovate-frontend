aaea78c894a6c36d8a233e6423735340205e2b36
gwynethguo <e0905362@u.nus.edu>
2023-08-02 17:28:01 +0800

    Validate form inputs

--- feminnovate-frontend/src/components/AuthenticationModal.jsx
+++ feminnovate-frontend/src/components/AuthenticationModal.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from "../assets/logo.svg";
 import hide from "../assets/hide.png";
 import show from "../assets/show.png";
 import close from "../assets/close.png";
 import styles from "../style";
+import axios from "axios";
 
 const CloseButton = ({ onClick }) => {
   return (
-    <button
-      className="w-3 sm:w-4 h-3 sm:h-4 m-auto -mt-3 -mr-4"
-      onClick={onClick}
-    >
+    <button className="w-3 sm:w-4 h-3 sm:h-4 m-auto -mr-1" onClick={onClick}>
       <img src={close} />
     </button>
   );
@@ -18,8 +16,8 @@ const CloseButton = ({ onClick }) => {
 
 const LogoWithTagline = () => {
   return (
-    <div className="flex flex-col w-[100%] items-center mb-9">
-      <img className="w-[75%]" src={logo} />
+    <div className="flex flex-col w-[100%] items-center mb-7">
+      <img className="w-[70%]" src={logo} />
       <div className={`${styles.subheading5} -mt-1`}>
         Building Careers For Women
       </div>
@@ -29,7 +27,7 @@ const LogoWithTagline = () => {
 
 const Input = ({ label, id, name, type, value, onChange }) => {
   return (
-    <div className="flex flex-col w-[100%] items-left mb-5">
+    <div className="flex flex-col w-[100%] items-left mb-4">
       <label
         htmlFor={id}
         className={`${styles.subheading5} -mt-1 text-black/50 w-full ml-1`}
@@ -42,7 +40,8 @@ const Input = ({ label, id, name, type, value, onChange }) => {
         type={type}
         value={value}
         onChange={onChange}
-        className="border border-grey rounded-md w-full h-14 px-3 py-2"
+        className="border border-grey rounded-md w-full h-12 px-3 py-2"
+        required
       ></input>
     </div>
   );
@@ -66,14 +65,15 @@ const PasswordInput = ({
       >
         {label}
       </label>
-      <div className="flex justify-center items-center mb-5 relative">
+      <div className="flex justify-center items-center mb-4 relative">
         <input
           id={id}
           name={name}
           type={type}
           value={value}
           onChange={onChange}
-          className="border border-grey rounded-md w-full h-14 pl-3 pr-12 py-2"
+          className="border border-grey rounded-md w-full h-12 pl-3 pr-12 py-2"
+          required
         ></input>
         <button
           className="absolute right-0 ml-1 mr-3 px-1 items-center"
@@ -112,23 +112,47 @@ const AuthenticationModal = ({
   handleOpenLogInModal,
   handleCloseModal,
 }) => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
+    validatePasswords(event.target.value, confirmPassword);
     setPassword(event.target.value);
   };
 
   const handleConfirmPasswordChange = (event) => {
+    validatePasswords(password, event.target.value);
     setConfirmPassword(event.target.value);
+  };
+
+  const validatePasswords = (password, confirmPassword) => {
+    setErrorMessage("");
+    if (password != "" && confirmPassword != "") {
+      if (password != confirmPassword) {
+        setErrorMessage("Password and Confirm Password doesn't match");
+      } else if (password.length < 8) {
+        setErrorMessage("Password must have at least 8 characters");
+      }
+    }
   };
 
   const togglePassword = () => {
@@ -158,47 +182,74 @@ const AuthenticationModal = ({
   };
 
   return (
-    <div className="flex flex-col w-full h-full bg-white rounded-3xl p-10 items-center justify-around">
+    <div className="flex flex-col w-full h-full bg-white rounded-3xl p-5 items-center justify-around">
       <div className="flex w-full h-full">
         <CloseButton onClick={handleCloseModal}></CloseButton>
       </div>
       <LogoWithTagline></LogoWithTagline>
-      <Input
-        label="Email"
-        id="email"
-        name="email"
-        type="email"
-        value={email}
-        onChange={handleEmailChange}
-      ></Input>
-      <PasswordInput
-        label="Password"
-        id="password"
-        name="password"
-        type={showPassword ? "text" : "password"}
-        value={password}
-        onChange={handlePasswordChange}
-        onClick={togglePassword}
-        showInput={showPassword ? 1 : 0}
-      ></PasswordInput>
-      {openSignUpModal && (
+      <form className="auth-form">
+        {openSignUpModal && (
+          <Input
+            label="Name"
+            id="name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          ></Input>
+        )}
+        {openSignUpModal && (
+          <Input
+            label="Username"
+            id="username"
+            name="username"
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          ></Input>
+        )}
+        <Input
+          label="Email"
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+        ></Input>
         <PasswordInput
-          label="Confirm Password"
-          id="confirmPassword"
-          name="confirmPassword"
-          type={showConfirmPassword ? "text" : "password"}
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-          onClick={toggleConfirmPassword}
-          showInput={showConfirmPassword ? 1 : 0}
+          label="Password"
+          id="password"
+          name="password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={handlePasswordChange}
+          onClick={togglePassword}
+          showInput={showPassword ? 1 : 0}
         ></PasswordInput>
-      )}
-      <button
-        className={`${styles.subheading4} bg-purple w-full px-12 py-3 my-5 border border-black rounded-full`}
-        type="button"
-      >
-        {openSignUpModal ? "Sign Up" : "Log In"}
-      </button>
+        {openSignUpModal && (
+          <PasswordInput
+            label="Confirm Password"
+            id="confirmPassword"
+            name="confirmPassword"
+            type={showConfirmPassword ? "text" : "password"}
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            onClick={toggleConfirmPassword}
+            showInput={showConfirmPassword ? 1 : 0}
+          ></PasswordInput>
+        )}
+        {errorMessage && (
+          <div className={`${styles.paragraph1} text-center text-pink`}>
+            {errorMessage}
+          </div>
+        )}
+        <button
+          className={`${styles.subheading4} bg-purple w-full px-12 py-3 my-4 border border-black rounded-full`}
+          type="submit"
+        >
+          {openSignUpModal ? "Sign Up" : "Log In"}
+        </button>
+      </form>
       <div className={`${styles.paragraph1}`}>
         {openSignUpModal ? (
           <SwitchMessage
