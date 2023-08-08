4c4a17156fe6482aaf74a5f01c3c16b2864c66bd
Jason Christopher <jason.christopher@u.nus.edu>
2023-08-08 20:09:56 +0800

    Add feature cards implementation

--- feminnovate-frontend/src/components/About.jsx
+++ feminnovate-frontend/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import FeatureCards from "./FeatureCards"
 
 const About = () => {
     return (
@@ -6,12 +7,16 @@ const About = () => {
             <div className="font-extrabold text-[44px] pt-12 pb-12 text-center">
                 About Feminnovate
             </div>
-            <div className="flex flex-col gap-10 text-[20px] font-medium text-center w-4/5 mx-auto">
+            <div className="flex flex-col gap-12 text-[20px] font-medium text-center w-4/5 mx-auto">
                 <p>
                     FemInnovate is a website that enables women to pursue or
                     advance their careers in STEM by providing them a platform
                     to look for job openings/events and obtain mentorship.
                 </p>
+                <p>
+                    Feminnovate offers the following features
+                </p>
+                <FeatureCards />
                 <p>
                     FemInnovate is submitted as part of TCS Sustainathon 2023.
                 </p>
---
+++ feminnovate-frontend/src/components/FeatureCards.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import landing_img from "../assets/landing-img.png"
+
+const data = [
+    {
+        image: landing_img,
+        text: "Job opportunities posting",
+    },
+    {
+        image: landing_img,
+        text: "Events posting for women in tech",
+    },
+    {
+        image: landing_img,
+        text: "Opportunities forum for women in STEM",
+    },
+]
+
+const FeatureCards = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+            {data.map((item) => <Card data={item} />)}
+        </div>
+    )
+}
+
+const Card = (props) => {
+    return (
+        <div className="flex flex-col gap-6 border-2 border-blue p-10 rounded-2xl">
+            <img src={props.data.image} />
+            <span>{props.data.text}</span>
+        </div>
+    )
+}
+export default FeatureCards
